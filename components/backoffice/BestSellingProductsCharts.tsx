8cbe950792f457d344c1684400a450da7e020ec4
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2025-06-14 23:22:40 +0500

    Added a pie chart to showcase some data

--- components/backoffice/BestSellingProductsCharts.tsx
+++ components/backoffice/BestSellingProductsCharts.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
 
-const BestSellingProductsCharts = () => {
-  return <div>BestSellingProductsCharts</div>;
-};
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function BestSellingProductsCharts() {
+  const data = {
+    labels: ["Cabbage", "Watermelon", "Broccoli", "Maize"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [50, 10, 20, 20],
+        backgroundColor: [
+          "rgba(0, 0, 255, 0.7)",
+          "rgba(255, 0, 221, 0.7)",
+          "rgba(2, 139,71, 0.7)",
+          "rgba(0, 0 ,0, 0.7)",
+        ],
+        borderColor: [
+          "rgba(0, 0, 255, 0.3)",
+          "rgba(255, 0, 221, 0.3)",
+          "rgba(2, 139,71, 0.3)",
+          "rgba(0, 0 ,0, 0.3)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <div className="rounded-lg bg-slate-700 p-8">
+      <h2 className="mb-4 text-xl font-bold">Best Selling Charts</h2>
+
+      {/*    Charts */}
+      <div className="p-4">
+        <Pie data={data} />
+      </div>
+    </div>
+  );
+}
 
 export default BestSellingProductsCharts;
